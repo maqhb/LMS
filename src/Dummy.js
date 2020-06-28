55f6a8502d359f6a6b5360f0f2c24afae8622db5
maqhb <31518888+maqhb@users.noreply.github.com>
2020-06-28 23:44:06 +0500

    third commit

--- src/Dummy.js
+++ src/Dummy.js
@@ -2,17 +2,17 @@ const Sections = [
     {
         teacher: "Teacher1",
         currSection: "BSSE6 A Morning",
-        students: [{name: "Std1",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A1"},{name: "Std2",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A2"},{name: "Std3",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A3",name: "Std1",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A1"},{name: "Std2",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A2"},{name: "Std3",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A3"}]
+        students: [{name: "Std1",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A1"},{name: "Std2",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A2"},{name: "Std3",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A3",name: "Std4",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A4"},{name: "Std5",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A5"},{name: "Std6",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A6"}]
     },
     {
         teacher: "Teacher2",
         currSection: "BSSE6 B Morning",
-        students: [{name: "Std4",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A4"},{name: "Std5",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A5"},{name: "Std6",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A6"}]
+        students: [{name: "Std7",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A7"},{name: "Std8",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A8"},{name: "Std9",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A9"}]
     },
     {
         teacher: "Teacher1",
         currSection: "BSSE6 A Evening",
-        students: [{name: "Std7",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A7"},{name: "Std7",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A7"},{name: "Std8",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A8"}]
+        students: [{name: "Std10",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A10"},{name: "Std11",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A11"},{name: "Std12",img: "https://portal.staralliance.com/cms/aux-pictures/prototype-images/avatar-default.png/@@images/image.png",reg: "A12"}]
     },
     
 ]
